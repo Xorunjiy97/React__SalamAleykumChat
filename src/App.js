49255677e8fed545e726d711614e296d83719af3
GereyevKamran <jsfront20@gmail.com>
2020-12-30 02:44:14 +0400

    messenger

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
-import './modules/style.less';
+// import './modules/style.less';
 import Login from './modules/login/Login';
+import Messenger from './modules/messenger/Messenger'
 
 function App() {
   return (
-    <Login/>
+    <div>
+        <Login/>
+        <Messenger/>
+    </div>
+
 );
 }
 
